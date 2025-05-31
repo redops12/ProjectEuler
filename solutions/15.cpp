b4e97d486e6990f4b23dbd5af951fc1cefa483e3
Robert Burke <rwburke@me.com>
2025-05-31 11:47:12 -0400

    removed test code from sol15

--- solutions/15.cpp
+++ solutions/15.cpp
@@ -11,14 +11,5 @@ int main(int argc, char * argv[]) {
     UNUSED(argc);
     UNUSED(argv);
 
-    __uint128_t test = 1;
-    for (int i = 0; i < 50; i++) {
-        __uint128_t sum = 0;
-        for (int j = 0; j <= i; j++) {
-            sum += n_choose_r(i, j);
-        }
-        assert(sum == test);
-        test *= 2;
-    }
     assert(n_choose_r(40,20) == 137846528820);
 }
